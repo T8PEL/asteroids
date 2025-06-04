8cf7960af3c0a2382be4b0bf9b37bb9219f2b287
T8PEL <elway.encinias413@gmail.com>
2025-06-04 00:59:11 -0600

    Added player rotation

--- main.py
+++ main.py
@@ -3,17 +3,26 @@
 # throughout this file :D
 import pygame
 from constants import *
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+from player import *
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        
+
+        #limit the framerate to 60fps
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
