ad46f3a836a33f276a66e4c7b10ee28c0a5b78d4
T8PEL <elway.encinias413@gmail.com>
2025-06-06 02:07:37 -0600

    added asteroids and asteroid field

--- main.py
+++ main.py
@@ -2,22 +2,39 @@
 # the open-source pygame library
 # throughout this file :D
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    
+    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (drawable, updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
+    asteroidfield = AsteroidField()
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for item in updatable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        
         pygame.display.flip()
         
 
